22c05924bbdcd43cd9be31b1b8cba4961b479f99
sarahbarberuk <sarahbarberuk@gmail.com>
2025-04-23 13:29:55 +0100

    Allowing CORS so that Next.js app can access endpoints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from app.routes import summarize, rewrite
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Next.js frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Include routers
 app.include_router(summarize.router)
